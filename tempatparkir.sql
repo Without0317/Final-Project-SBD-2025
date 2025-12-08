2569748a903a83b1fc582a0f8717ac640cc38e3d
Khen Patra <148628723+Without0317@users.noreply.github.com>
2025-12-08 19:49:36 +0700

    Add total slots and update staff IDs in SQL insert

--- tempatparkir.sql
+++ tempatparkir.sql
@@ -1,54 +1,55 @@
-insert into tempat_parkir (id_tempat_parkir, lokasi, jenis_tempat_parkir, jenis_k_tempat_parkir, id_staff) values
+
+insert into tempat_parkir (id_tempat_parkir, nama_tempat_parkir, jenis_tempat_parkir, jenis_k_tempat_parkir, total_slot_tempat_parkir, staff_id_staff) values
 -- PARKIRAN TC
-('TP0001', 'Parkiran TC', 'MH', 'MOTOR', 'S00001'),
-('TP0002', 'Parkiran TC', 'MH', 'MOBIL', 'S00002'),
-('TP0003', 'Parkiran TC', 'TD', 'MOTOR', 'S00003'),
-('TP0004', 'Parkiran TC', 'TD', 'MOBIL', 'S00004'),
-('TP0005', 'Parkiran TC', 'UM', 'MOTOR', 'S00005'),
-('TP0006', 'Parkiran TC', 'UM', 'MOBIL', 'S00006'),
+('TP0001', 'Parkiran TC', 'MH', 'MOTOR', 40, 'S00001'),
+('TP0002', 'Parkiran TC', 'MH', 'MOBIL', 20, 'S00002'),
+('TP0003', 'Parkiran TC', 'TD', 'MOTOR', 40, 'S00003'),
+('TP0004', 'Parkiran TC', 'TD', 'MOBIL', 20, 'S00004'),
+('TP0005', 'Parkiran TC', 'UM', 'MOTOR', 40, 'S00005'),
+('TP0006', 'Parkiran TC', 'UM', 'MOBIL', 20, 'S00006'),
 
 -- PARKIRAN ELEKTRO
-('TP0007', 'Parkiran Elektro', 'MH', 'MOTOR', 'S00007'),
-('TP0008', 'Parkiran Elektro', 'MH', 'MOBIL', 'S00008'),
-('TP0009', 'Parkiran Elektro', 'TD', 'MOTOR', 'S00009'),
-('TP0010', 'Parkiran Elektro', 'TD', 'MOBIL', 'S00010'),
-('TP0011', 'Parkiran Elektro', 'UM', 'MOTOR', 'S00011'),
-('TP0012', 'Parkiran Elektro', 'UM', 'MOBIL', 'S00012'),
+('TP0007', 'Parkiran Elektro', 'MH', 'MOTOR', 40,  'S00007'),
+('TP0008', 'Parkiran Elektro', 'MH', 'MOBIL', 20,  'S00008'),
+('TP0009', 'Parkiran Elektro', 'TD', 'MOTOR', 40,  'S00009'),
+('TP0010', 'Parkiran Elektro', 'TD', 'MOBIL', 20,  'S00010'),
+('TP0011', 'Parkiran Elektro', 'UM', 'MOTOR', 40,  'S00011'),
+('TP0012', 'Parkiran Elektro', 'UM', 'MOBIL', 20,  'S00012'),
 
 -- PARKIRAN SISTEM INFORMASI
-('TP0013', 'Parkiran Sistem Informasi', 'MH', 'MOTOR', 'S00013'),
-('TP0014', 'Parkiran Sistem Informasi', 'MH', 'MOBIL', 'S00014'),
-('TP0015', 'Parkiran Sistem Informasi', 'TD', 'MOTOR', 'S00015'),
-('TP0016', 'Parkiran Sistem Informasi', 'TD', 'MOBIL', 'S00016'),
-('TP0017', 'Parkiran Sistem Informasi', 'UM', 'MOTOR', 'S00017'),
-('TP0018', 'Parkiran Sistem Informasi', 'UM', 'MOBIL', 'S00018'),
+('TP0013', 'Parkiran sisfor', 'MH', 'MOTOR', 40,  'S00013'),
+('TP0014', 'Parkiran sisfor', 'MH', 'MOBIL', 20,  'S00014'),
+('TP0015', 'Parkiran sisfor', 'TD', 'MOTOR', 40,  'S00015'),
+('TP0016', 'Parkiran sisfor', 'TD', 'MOBIL', 20,  'S00016'),
+('TP0017', 'Parkiran sisfor', 'UM', 'MOTOR', 40,  'S00017'),
+('TP0018', 'Parkiran sisfor', 'UM', 'MOBIL', 20,  'S00018'),
 
 -- KPA 1
-('TP0019', 'KPA 1', 'MH', 'MOTOR', 'S00019'),
-('TP0020', 'KPA 1', 'MH', 'MOBIL', 'S00020'),
-('TP0021', 'KPA 1', 'TD', 'MOTOR', 'S00021'),
-('TP0022', 'KPA 1', 'TD', 'MOBIL', 'S00022'),
-('TP0023', 'KPA 1', 'UM', 'MOTOR', 'S00023'),
-('TP0024', 'KPA 1', 'UM', 'MOBIL', 'S00024'),
+('TP0019', 'KPA 1', 'MH', 'MOTOR', 40, 'S00019'),
+('TP0020', 'KPA 1', 'MH', 'MOBIL', 20, 'S00020'),
+('TP0021', 'KPA 1', 'TD', 'MOTOR', 40, 'S00021'),
+('TP0022', 'KPA 1', 'TD', 'MOBIL', 20, 'S00022'),
+('TP0023', 'KPA 1', 'UM', 'MOTOR', 40, 'S00023'),
+('TP0024', 'KPA 1', 'UM', 'MOBIL', 20, 'S00024'),
 
 -- KPA 2
-('TP0025', 'KPA 2', 'MH', 'MOTOR', 'S00025'),
-('TP0026', 'KPA 2', 'MH', 'MOBIL', 'S00026'),
-('TP0027', 'KPA 2', 'TD', 'MOTOR', 'S00027'),
-('TP0028', 'KPA 2', 'TD', 'MOBIL', 'S00028'),
-('TP0029', 'KPA 2', 'UM', 'MOTOR', 'S00029'),
-('TP0030', 'KPA 2', 'UM', 'MOBIL', 'S00030'),
+('TP0025', 'KPA 2', 'MH', 'MOTOR', 40, 'S00025'),
+('TP0026', 'KPA 2', 'MH', 'MOBIL', 20, 'S00026'),
+('TP0027', 'KPA 2', 'TD', 'MOTOR', 40, 'S00027'),
+('TP0028', 'KPA 2', 'TD', 'MOBIL', 20, 'S00028'),
+('TP0029', 'KPA 2', 'UM', 'MOTOR', 40, 'S00029'),
+('TP0030', 'KPA 2', 'UM', 'MOBIL', 20, 'S00030'),
 
 -- KPA 3
-('TP0031', 'KPA 3', 'MH', 'MOTOR', 'S00031'),
-('TP0032', 'KPA 3', 'MH', 'MOBIL', 'S00032'),
-('TP0033', 'KPA 3', 'TD', 'MOTOR', 'S00033'),
-('TP0034', 'KPA 3', 'TD', 'MOBIL', 'S00034'),
-('TP0035', 'KPA 3', 'UM', 'MOTOR', 'S00035'),
-('TP0036', 'KPA 3', 'UM', 'MOBIL', 'S00036'),
+('TP0031', 'KPA 3', 'MH', 'MOTOR', 40, 'S00031'),
+('TP0032', 'KPA 3', 'MH', 'MOBIL', 20, 'S00032'),
+('TP0033', 'KPA 3', 'TD', 'MOTOR', 40, 'S00033'),
+('TP0034', 'KPA 3', 'TD', 'MOBIL', 20, 'S00034'),
+('TP0035', 'KPA 3', 'UM', 'MOTOR', 40, 'S00035'),
+('TP0036', 'KPA 3', 'UM', 'MOBIL', 20, 'S00036'),
 
 --Asrama
-('TP0037', 'Asrama', 'MH', 'MOTOR', 'S00037'),
-('TP0038', 'Asrama', 'MH', 'MOBIL', 'S00038'),
-('TP0039', 'Asrama', 'UM', 'MOTOR', 'S00039'),
-('TP0040', 'Asrama', 'UM', 'MOBIL', 'S00040');
+('TP0037', 'Asrama', 'MH', 'MOTOR', 40, 'S00037'),
+('TP0038', 'Asrama', 'MH', 'MOBIL', 20, 'S00038'),
+('TP0039', 'Asrama', 'UM', 'MOTOR', 40, 'S00039'),
+('TP0040', 'Asrama', 'UM', 'MOBIL', 20, 'S00040');
